a42a90ac4a84bae856ec264ed4805c44da3e60be
robo-mop <realaryanpingle@gmail.com>
2023-09-29 15:29:18 +0530

    Add more functionality

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 let byteArray = null;
+const representations = [
+    document.querySelector(".representation--left"),
+    document.querySelector(".representation--right")
+];
+
+/**
+ * @returns {HTMLElement[]}
+ */
+function querySelectorAll(selectors, target=document) {
+    return Array.from(target.querySelectorAll(selectors));
+}
 
 document.querySelector("#input-file").onchange = function() {
     let file = this.files[0];
 
     selectFile(file);
+}
+
+/**
+ * Groups each consecutive sequence of `N` values with `connector`, and returns this new array
+ * @param {Array} arr 
+ * @param {number} N 
+ * @param {string} connector 
+ */
+function joinEvery(arr, N, connector) {
+    const result = [];
+    for(let i = 0; i <= (arr.length-1) / N; ++i) {
+        result.push(arr.slice(i*N, Math.min(arr.length, (i+1)*N)).join(connector));
+    }
+    return result;
 }
 
 /**
@@ -14,13 +39,36 @@ function selectFile(file) {
 
     reader.onload = function(event) {
         byteArray = new Uint8Array(event.target.result);
-        const NUMBER_OF_BYTES = 100;
 
-        const hexString = Array.from(byteArray.slice(0, NUMBER_OF_BYTES), byte => byte.toString(16).padStart(2, '0')).join(' ');
-        document.getElementById('bytes').innerHTML = hexString;
+        setupBothRepresentations();
     };
 
     reader.readAsArrayBuffer(file);
+}
+
+function setupBothRepresentations() {
+    const NUMBER_OF_BYTES = 0x120;
+    const pruned = byteArray.slice(0, NUMBER_OF_BYTES);
+    
+    representations[0].querySelector(".bytes").innerHTML = joinEvery(toByteString(pruned), 16, " ").join("<br>").replace(/\s/g, "&nbsp;");
+    representations[1].querySelector(".bytes").innerHTML = joinEvery(toASCIIString(pruned), 16, " ").join("<br>").replace(/\s/g, "&nbsp;");
+}
+
+/**
+ * @param {Array} pruned 
+ * @returns {Array}
+ */
+function toByteString(pruned) {
+    return Array.from(pruned).map(byte => byte.toString(16).padStart(2, '0'));
+}
+
+/**
+ * @param {Array} pruned 
+ * @returns {Array}
+ */
+function toASCIIString(pruned) {
+    const decoder = new TextDecoder('utf-8');
+    return Array.from(decoder.decode(pruned));
 }
 
 /**
